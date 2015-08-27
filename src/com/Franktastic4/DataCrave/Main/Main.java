acd5ef8671afc4486544de7965597b4b0bcdd9d3
Franktastin4 <frankchung279@gmail.com>
2015-08-27 09:56:35 -0700

    Saving before adding hashmap

--- src/com/Franktastic4/DataCrave/Main/Main.java
+++ src/com/Franktastic4/DataCrave/Main/Main.java
@@ -1,7 +1,11 @@
 package com.Franktastic4.DataCrave.Main;
 
+import java.awt.List;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
@@ -9,12 +13,12 @@ import javax.swing.SwingUtilities;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
 import java.sql.*;
-
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class Main {
 
 	private ActionListener searchButtonListener;
-	static final NutritionTableDbHelper myDbHelper = new NutritionTableDbHelper();
 
 	public static void SelectDatabase(){
 		
@@ -23,17 +27,21 @@ public class Main {
 		   
 		try{
 			// Register JDBC driver
-		    Class.forName(myDbHelper.FOR_NAME);
+		   Class.forName(NutritionTableDbHelper.FOR_NAME);
 
 		    // Open a connection (Select database)
 		    System.out.println("Connecting to database...");
-		    databaseConnection = DriverManager.getConnection(myDbHelper.DB_URL);
-
-		    // If no database is selected, create it and load in from CSV file.
-		    if(!databaseExists(databaseConnection)){
-		       System.out.println("Database does not exist");
-		       createDatabase(databaseConnection); 
-		     }
+		    databaseConnection = DriverManager.getConnection(NutritionTableDbHelper.DB_URL,
+		    		NutritionTableDbHelper.USER, NutritionTableDbHelper.PASS);
+		    
+		    // Check if table exists
+		    if(!tableExists(databaseConnection)){
+		       System.out.println("Table does not exist");
+		       createTable(databaseConnection); 
+		       fillTable(databaseConnection);
+		    }else{
+		    	System.out.println("Table already exists");
+		    }
 		    	       
 		   }catch(SQLException se){
 			      se.printStackTrace();
@@ -50,55 +58,153 @@ public class Main {
 
 	}
 		   
-    public static Boolean databaseExists(Connection databaseConnection){
+	public static void dropTable(Connection databaseConnection) throws SQLException{
+		
+		Statement statement = databaseConnection.createStatement();
+		statement.executeUpdate("DROP TABLE " + NutritionTableDbHelper.TABLE_NAME);
+		statement.close();
+		
+	}
+	
+    public static Boolean tableExists(Connection databaseConnection){
 	   
-	   try{
-		   ResultSet resultSet = databaseConnection.getMetaData().getCatalogs();
-		   		while (resultSet.next()) {		        
-		   			String databaseName = resultSet.getString(1);
-		   			if(databaseName.equals(myDbHelper.TABLE_NAME)){
-		   				return true;
-		   			}
-		   		}
-		   		resultSet.close();
-	   }catch(SQLException se){
-		   se.printStackTrace();
-	   }
-	   
-	   return false;
+	 try {
+		 ResultSet tables = databaseConnection.getMetaData().getTables(null, null, NutritionTableDbHelper.TABLE_NAME, null);
+		 if(tables.next()){
+			 return true;
+		 }
+		
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	 
+	 
+	 return false;
+	 
+		   
    }
    
-    public static void createDatabase(Connection databaseConnection){
-	   
-	   // Create database
-	   
+    public static void createTable(Connection databaseConnection) throws IOException{
+
+	      
 	   try{
 	      Statement statement = databaseConnection.createStatement();
-	      statement.executeUpdate(myDbHelper.CREATE_DATABASE);
-	      System.out.println("Created database");
-	      
+	      statement.executeUpdate(NutritionTableDbHelper.CREATE_TABLE);
+	       
 	   }catch(Exception se){
 		   se.printStackTrace();
 	   }
-	   
-	   // import USDA info into database
-	   
-	   CSVReader reader = new CSVReader(new FileReader("data.csv"), ',' , '"' , 1);
-	      
-	   String[] nextLine;
-	   while ((nextLine = reader.readNext()) != null) {
-	      if (nextLine != null) {
-	         
-	    	  //Verifying the read data here
-	         System.out.println(Arrays.toString(nextLine));
-	      }
-	    }
-	   
    }
 	
+    public static void fillTable(Connection databaseConnection){
+    	String sql = "temp";
+    	// import USDA info into database
+ 	   try{
+ 		  
+ 		  Statement statement = databaseConnection.createStatement();  
+ 		  CSVReader reader = new CSVReader(new FileReader("USDA_DATA1.csv"), ',', '"', 1);
+ 	      
+ 		  String[] nextLine;
+ 	      while ((nextLine = reader.readNext()) != null) {
+ 	         if (nextLine != null) {
+ 	      	 
+ 	        	 // Check each line
+ 	        	 for(int index = 3; index < 10; index++){
+
+ 	        		 // fill the blanks
+ 	        		 if(nextLine[index].isEmpty()){
+ 	        			 nextLine[index] = "0";
+ 	        		 }        	
+ 	        		 
+ 	        	 }
+
+ 	        	 // add to database
+ 	        	  sql = 
+ 	        			"INSERT INTO " + NutritionTableDbHelper.TABLE_NAME + 
+ 	        			" VALUES ('"  + nextLine[1]	 + "', "	+
+ 	        			Double.parseDouble(nextLine[3]) + ", " + 
+ 	        			Double.parseDouble(nextLine[4]) + ", " + 		
+ 	        			Double.parseDouble(nextLine[5]) + ", " + 	
+ 	        			Double.parseDouble(nextLine[7]) + ", " + 			
+ 	        			Double.parseDouble(nextLine[9]) +  ") ";
+	
+ 	        	 statement.executeUpdate(sql);
+ 	        	 
+ 	         }
+ 	       }
+ 	 	  
+ 		  
+ 		  
+ 	 	  reader.close();
+ 	 	  statement.close();
+ 	 	   
+ 	   }catch(FileNotFoundException e){
+ 		   e.printStackTrace();
+ 	   }catch(IOException e){
+ 		   e.printStackTrace();
+ 	   }catch(SQLSyntaxErrorException e){
+ 		  System.out.println(sql);
+ 	   }catch(Exception e){
+ 		   e.printStackTrace();
+    }   
+	   
+    }
+    
+    public static void buildHashmap(){
+   	
+    	try{
+    		
+    		System.out.println("Connecting to database to print.");
+		    Connection databaseConnection = DriverManager.getConnection(NutritionTableDbHelper.DB_URL,
+		    		NutritionTableDbHelper.USER, NutritionTableDbHelper.PASS);
+    		Statement statement = databaseConnection.createStatement();
+    		
+    		
+    		
+    		// Build Query
+    		String query = "SELECT " + 
+    		NutritionTableDbHelper.NAME + ", " + 
+    		NutritionTableDbHelper.CALORIES + ", " +
+    		NutritionTableDbHelper.PROTEIN + ", " +
+    		NutritionTableDbHelper.FAT + ", " +
+    		NutritionTableDbHelper.CARBS + ", " +
+    		NutritionTableDbHelper.SUGAR + 
+    		" FROM " +  NutritionTableDbHelper.TABLE_NAME; 
+    		
+    		// Submit query, store in ResultSet object
+    		ResultSet results = statement.executeQuery(query);
+    		
+    		// Store result in hashmap to improve search time over DB
+    		// hashmap over hashtable because a hashmap works faster
+    		// because its unsychronized so it doesn't need to be checked
+    		
+    		HashMap hashmapUSDA = new HashMap();
+    		
+    		while(results.next()){
+    			System.out.println(
+    					"Name: " + results.getString(NutritionTableDbHelper.NAME) + 
+    					" Protein: " + results.getInt(NutritionTableDbHelper.PROTEIN)
+    					);
+    			
+    		}
+    	
+    	}catch(SQLException e){
+    		e.printStackTrace();
+    	}
+    	
+    	
+    	
+    }
+    
 	public static void main(String[] args){
 		
+		// The database closes the connection at the end of SelectDatabase? Doesn't seem to return.
 		SelectDatabase();
+		buildHashmap();
+		
+		// Then Make Hashmap by selecting Table in the DATABASE
+		// Then build the Tree
 		
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run(){
--- src/com/Franktastic4/DataCrave/Main/NutritionTableDbHelper.java
+++ src/com/Franktastic4/DataCrave/Main/NutritionTableDbHelper.java
@@ -7,9 +7,46 @@ public class NutritionTableDbHelper{
 	}
 
 	public static final String TABLE_NAME = "NUTRITION_TABLE";
-	public static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-	public static final String DB_URL = "jdbc:mysql://localhost/EMP";
-	public static final String FOR_NAME = "com.mysql.jdbc.Driver";
-	public static final String CREATE_DATABASE = "CREATE DATABASE NUTRITION_TABLE";
+	public static final String DATABASE_NAME = "USDA_DATABASE";
+	
+	public static final String FOR_NAME = "org.apache.derby.jdbc.EmbeddedDriver";  
+	public static final String DB_URL = "jdbc:derby:" + DATABASE_NAME + ";create=true";
+	
+	public static final String USER = "username";
+    public static final String PASS = "password";
+	
+	public static final String CREATE_DATABASE = "CREATE DATABASE " + DATABASE_NAME;
+	
+	// Column B (1)
+	public static final String NAME = "NAME";
+	
+	// Column D (3) 
+	public static final String CALORIES = "CALORIES";
+	
+	// Column E (4)
+	public static final String PROTEIN = "PROTEIN";
+	
+	// Column F (5)
+	public static final String FAT = "FAT";
+	
+	// Column H (7)
+	public static final String CARBS = "CARBOHYDRATES";
+	
+	// Column J (9)
+	public static final String SUGAR = "SUGAR";
+
+	public static final String INT = " INTEGER, ";
+	
+	public static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + " (" + 
+											  NAME + " VARCHAR(255),  " + 
+											  CALORIES + INT +
+											  PROTEIN + INT + 
+											  FAT + INT + 
+											  CARBS + INT +
+											  SUGAR + INT + 
+											  "PRIMARY KEY " + "( " + NAME + ")" +  
+											  ")";
+	
+
 
 }
